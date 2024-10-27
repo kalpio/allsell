a1735761eb6e244a820b29f7b6aad412f05426a0
kalpio <piotr.lkalina@gmail.com>
2024-10-27 23:39:59 +0100

    moved User struct

--- src/types/user.go
+++ src/types/user.go
@@ -4,8 +4,20 @@ import (
 	"errors"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	is "github.com/go-ozzo/ozzo-validation/v4/is"
+	"github.com/google/uuid"
 	"strings"
 )
+
+type User struct {
+	ID       uuid.UUID `json:"id" form:"id"`
+	Name     string    `json:"name" form:"name"`
+	Email    string    `json:"email" form:"email"`
+	Password string    `json:"password" form:"password"`
+}
+
+func NewUser(name, email, password string) User {
+	return User{uuid.New(), name, email, password}
+}
 
 type UserRegister struct {
 	UserName        string `form:"username"`
